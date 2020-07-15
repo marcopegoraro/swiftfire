a26ee7fa8d92eecfc692f337abdc4da80a94fd2a
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-15 19:46:32 +0200

    Added skeletons for enablement and firing rules.

---
+++ swiftfire/semantics/enablement_rules/petrinets_enablement_rules.py
@@ -0,0 +1,8 @@
+class EnablementRule:
+    def enabled_transitions(self, net):
+        pass
+
+
+class EnablementRuleInhibitorArcs(EnablementRule):
+    def enabled_transitions(self, net):
+        pass
---
+++ swiftfire/semantics/firing_rules/petrinets_firing_rules.py
@@ -0,0 +1,9 @@
+
+class FiringRule:
+    def fire(self, net, transition):
+        pass
+
+
+class FiringRuleResetArcs(FiringRule):
+    def fire(self, net, transition):
+        pass
