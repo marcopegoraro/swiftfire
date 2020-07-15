2b49f0855df7cade79d5aee7b264e92da60669e2
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-15 20:04:40 +0200

    Added setters and getter to  the Petri net class.

--- swiftfire/artifacts/nets/petri_net.py
+++ swiftfire/artifacts/nets/petri_net.py
@@ -15,6 +15,42 @@ class PetriNet:
         self.__enablement_rule = petrinet_enablement_rules.EnablementRule if inhibitor_arcs is None else petrinet_enablement_rules.EnablementRuleInhibitorArcs
         self.__firing_rule = petrinet_firing_rules.FiringRule if reset_arcs is None else petrinet_firing_rules.FiringRuleResetArcs
 
+    def __get_graph(self):
+        return self.__graph
+
+    def __set_graph(self, graph):
+        self.__graph = graph
+
+    def __get_inhibitor_arcs(self):
+        return self.__inhibitor_arcs
+
+    def __set_inhibitor_arcs(self, inhibitor_arcs):
+        self.__inhibitor_arcs = inhibitor_arcs
+
+    def __get_reset_arcs(self):
+        return self.__reset_arcs
+
+    def __set_reset_arcs(self, reset_arcs):
+        self.__reset_arcs = reset_arcs
+
+    def __get_enablement_rule(self):
+        return self.__enablement_rule
+
+    def __set_enablement_rule(self, enablement_rule):
+        self.__enablement_rule = enablement_rule
+
+    def __get_firing_rule(self):
+        return self.__firing_rule
+
+    def __set_firing_rule(self, firing_rule):
+        self.__firing_rule = firing_rule
+
+    graph = property(__get_graph, __set_graph)
+    inhibitor_arcs = property(__get_inhibitor_arcs, __set_inhibitor_arcs)
+    reset_arcs = property(__get_reset_arcs, __set_reset_arcs)
+    enablement_rule = property(__get_enablement_rule, __set_enablement_rule)
+    firing_rule = property(__get_firing_rule, __set_firing_rule)
+
     def __getitem__(self, key):
         return self.__graph[key]
 
