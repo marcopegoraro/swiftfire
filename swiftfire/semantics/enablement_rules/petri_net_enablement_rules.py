055e507d7fe0b9d91aa2e74644979d375e1c072a
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-17 19:46:50 +0200

    Added basic enablement rule.

--- swiftfire/semantics/enablement_rules/petri_net_enablement_rules.py
+++ swiftfire/semantics/enablement_rules/petri_net_enablement_rules.py
@@ -1,6 +1,16 @@
 class EnablementRule:
     def enabled_transitions(self, net, marking):
-        pass
+        # return {transition for transition in net.transitions if min(marking[place] for place in net.graph.degree(type="in")[transition]) > 0}
+        enabled_transitions = set()
+        for transition in net.transitions:
+            enabled = True
+            for place in net.graph.degree(type="in")[transition]:
+                if marking[place] < 1:
+                    enabled = False
+                    break
+            if enabled:
+                enabled_transitions.add(transition)
+        return enabled_transitions
 
 
 class EnablementRuleInhibitorArcs(EnablementRule):
