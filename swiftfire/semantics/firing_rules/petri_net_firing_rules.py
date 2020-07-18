a24a2ac58038cb87259fcc7414d414e323d9ba0b
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-18 11:54:44 +0200

    Written firing rule.

--- swiftfire/semantics/firing_rules/petri_net_firing_rules.py
+++ swiftfire/semantics/firing_rules/petri_net_firing_rules.py
@@ -1,8 +1,21 @@
+class TransitionNotEnabledError(ValueError):
+    # TODO: move to a more appropriate place
+    pass
+
+
 class FiringRule:
     def fire(self, net, marking, transition):
-        pass
+        if net.enablement_rule.is_enabled(net, transition, marking):
+            for place in net.preset(transition):
+                marking[place] -= 1
+            for place in net.postset(transition):
+                marking[place] += 1
+            return marking
+        else:
+            raise TransitionNotEnabledError()
 
 
 class FiringRuleResetArcs(FiringRule):
     def fire(self, net, marking, transition):
-        pass
+        # TODO: implement
+        raise NotImplementedError
