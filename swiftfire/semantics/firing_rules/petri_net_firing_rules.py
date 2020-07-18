478afcdeb743af94580ee54211f6ac2897617712
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-18 17:13:16 +0200

    Debugged firing rules.

--- swiftfire/semantics/firing_rules/petri_net_firing_rules.py
+++ swiftfire/semantics/firing_rules/petri_net_firing_rules.py
@@ -7,11 +7,14 @@ class FiringRule:
 
     @staticmethod
     def fire(net, marking, transition):
-        if net.enablement_rule.is_enabled(net, transition, marking):
+        if net.enablement_rule.is_enabled(net, marking, transition):
             for place in net.preset(transition):
                 marking[place] -= 1
             for place in net.postset(transition):
-                marking[place] += 1
+                if place in marking:
+                    marking[place] += 1
+                else:
+                    marking[place] = 1
             return marking
         else:
             raise TransitionNotEnabledError()
