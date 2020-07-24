f02e60c844535b39d52436a99ce1ad8af99f780b
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-07-24 14:39:52 +0200

    Added identifiers for configuration members.

--- swiftfire/identifiers.py
+++ swiftfire/identifiers.py
@@ -1,3 +1,14 @@
 # TODO: move these definitions in a proper configuration file
 
+### LabeledPetriNet
+# Identifier of transition labels
 LABEL_ID = 'label'
+
+### SystemNet
+# Identifier of the default configuration of markings
+DEFAULT_CONFIG = 'default'
+# Keys of the data fields for marking configurations in system nets
+INITIAL_MARKING = 'initial_marking'
+CURRENT_MARKING = 'marking'
+ENABLED_TRANSITIONS = 'enabled_transitions'
+FINAL_MARKINGS = 'final_markings'
